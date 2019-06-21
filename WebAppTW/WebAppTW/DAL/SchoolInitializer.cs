6ee32172c7fd05cff81904f059f253dbab194ff2
TW-PC\Tw <thiagowaltrick@gmail.com>
2019-06-21 18:57:49 -0300

    part 1

---
+++ WebAppTW/WebAppTW/DAL/SchoolInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebAppTW.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        // Na pasta DAL, crie um novo arquivo de classe chamado SchoolInitializer.cs e substitua o código de modelo pelo código a seguir, que faz com que um banco de dados a ser criado quando necessário e carrega dados de teste no novo banco de dados.
+    }
+}
